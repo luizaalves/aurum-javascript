dfca8afe7545d0518c5107fc0f7bf3db05d3d8ea
Luiza <l.uhzinha@hotmail.com>
2020-12-02 20:43:41 -0300

    iniciando repositório

---
+++ src/exer1.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} string  
+ * @param {number} limit 
+ * @return {string}  
+ */
+const removeRepeated = (string, limit) => {
+    //Your logic goes here
+    return null;
+}
+
+export default removeRepeated;
