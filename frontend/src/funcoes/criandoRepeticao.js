ed48d09c0ddafd1e9c948002c85ff3b365703cff
Luiza <l.uhzinha@hotmail.com>
2020-12-04 16:45:07 -0300

    exercicio 2 - testado

--- frontend/src/funcoes/criandoRepeticao.js
+++ frontend/src/funcoes/criandoRepeticao.js
@@ -4,8 +4,35 @@
  * @return {string}  
  */
 const replaceChar = (string, times) => {
-    //Your logic goes here
-    return null;
+    for(let i=0;i<string.length;i++){
+        if(string[i]==='?'){
+            if(i===0){
+                string = string.substring(1);
+                console.log(string[string.length-1]);
+                for(let k=1;k<string.length;k++){
+                    if(string[string.length-k]!=='?'){
+                        var strTemp='';
+                        for(let j=0;j<times;j++){
+                            strTemp=strTemp + string[string.length-k];
+                        }
+                        string=strTemp+string;
+                        break;
+                    }
+                }
+                
+            }else{
+                var pieceOne=string.substring(0,i);
+                var pieceTwo=string.substring(i+1,string.length);
+                for(let j=0;j<times;j++){
+                    pieceOne=pieceOne+ string[i-1];
+                }
+                string = pieceOne+pieceTwo;
+                console.log(string);
+            }
+            
+        }
+    }
+    return string;
 }
 
 export default replaceChar;
